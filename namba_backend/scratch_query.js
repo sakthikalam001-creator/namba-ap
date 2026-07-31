43fd6dd15134e40aac610222d6f8a0332c09cad7
Namba Dev <dev@namba.com>
2026-07-31 15:33:00 +0530

    Remove scratch query script

--- namba_backend/scratch_query.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/namba').then(async () => {
-  const list = await mongoose.connection.db.collection('vendors').find({}).toArray();
-  console.log(JSON.stringify(list, null, 2));
-  process.exit(0);
-}).catch(err => {
-  console.error(err);
-  process.exit(1);
-});
