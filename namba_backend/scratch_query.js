200857da82e2c1b5d375022be7660a82bfbcd20a
Namba Dev <dev@namba.com>
2026-07-31 16:56:04 +0530

    Inspect vendor tokens in database

--- namba_backend/scratch_query.js
+++ namba_backend/scratch_query.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/namba').then(async () => {
-  const list = await mongoose.connection.db.collection('orders').find({}).sort({createdAt: -1}).limit(5).toArray();
-  console.log(JSON.stringify(list, null, 2));
+  const vendor = await mongoose.connection.db.collection('vendors').findOne({_id: new mongoose.Types.ObjectId('6a57aefb16962c32adc0341c')});
+  console.log("VENDOR DATA:");
+  console.log(JSON.stringify(vendor, null, 2));
   process.exit(0);
 }).catch(err => {
   console.error(err);
