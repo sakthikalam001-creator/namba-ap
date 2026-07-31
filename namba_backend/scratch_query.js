31850a15c6a568b78b0658f4781bd7c4672cdbd2
Namba Dev <dev@namba.com>
2026-07-31 15:31:58 +0530

    Query namba database instead of namba_db

--- namba_backend/scratch_query.js
+++ namba_backend/scratch_query.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/namba_db').then(async () => {
+mongoose.connect('mongodb://localhost:27017/namba').then(async () => {
   const list = await mongoose.connection.db.collection('vendors').find({}).toArray();
   console.log(JSON.stringify(list, null, 2));
   process.exit(0);
