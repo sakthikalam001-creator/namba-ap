10e7d83b3d18096032a12241cf837df2dacac6a7
Namba Dev <dev@namba.com>
2026-07-31 15:31:17 +0530

    List all vendors in scratch query

--- namba_backend/scratch_query.js
+++ namba_backend/scratch_query.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/namba_db').then(async () => {
-  const { ObjectId } = require('mongodb');
-  const v = await mongoose.connection.db.collection('vendors').findOne({_id: new ObjectId('6a57aefb16962c32adc0341c')});
-  console.log(JSON.stringify(v, null, 2));
+  const list = await mongoose.connection.db.collection('vendors').find({}).toArray();
+  console.log(JSON.stringify(list, null, 2));
   process.exit(0);
 }).catch(err => {
   console.error(err);
